fcfba80bd88b4734ea0920f49c6dc257b3d28c59
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-01-11 04:36:59 +0000

    Tested with console.log

--- errors-and-debugging/exercises/DebuggingLogicErrors2.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors2.js
@@ -12,9 +12,11 @@ let fuelLevel = 17000;
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
    launchReady = true;
+   console.log(launchReady);
 } else {
    console.log('WARNING: Insufficient fuel!');
    launchReady = false;
+   console.log(launchReady);
 }
 
 // if (crewStatus && computerStatus === 'green'){
