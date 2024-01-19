6be2519aea6482d4dc312a015b853a2d292e0747
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-01-19 01:36:48 +0000

    studio

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -8,8 +8,11 @@ console.log(strTwo);
 //Use a template literal to print the original and modified string in a descriptive phrase.
 console.log(`${str} "looks like" ${strTwo}`);
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let n = 3
+n = input.question("How many letters do you want relocated?");
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
-if () {
-} else {
+if (n <= 9) { strTwo = str.slice(n) + str.slice(0,n);
+    console.log(strTwo);
+} else { console.log(` ${n} is too many letters to relocate. Pick a smaller number.`);
 }
