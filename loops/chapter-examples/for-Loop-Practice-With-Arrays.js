771ae3e19d5b4a576dcb6aed000c369eb02b5f00
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-01-22 20:30:16 +0000

    done

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,8 @@
 // create an array variable containing the names
 
 // write a for loop that prints each name on a different line
+let fam = ["Kate", "David", "Zelda", "Midna", "Kairi"];
+
+for (let i = 0; i < fam.length; i++) {
+   console.log(fam[i]);
+}
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,7 +1,7 @@
 // Create a string variable containing your name.
-const name = 'Katelyn Erlebacher';
+const word = 'Katelyn Erlebacher';
 
 // Write a for loop that prints each character in your name on a different line.
-for (let i = 0; i < name.lenth; i++) {
-    console.log(name[i]);
+for (let i = 0; i < word.length; i++) {
+    console.log(word[i]);
 }
