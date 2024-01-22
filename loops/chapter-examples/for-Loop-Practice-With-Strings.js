fd1f2c77a6bb14231d86cfd21e89081fecb8a608
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-01-22 18:20:10 +0000

    commit

--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,7 @@
 // Create a string variable containing your name.
-
+const name = 'Katelyn Erlebacher';
 
 // Write a for loop that prints each character in your name on a different line.
+for (let i = 0; i < name.lenth; i++) {
+    console.log(name[i]);
+}
