8631b8a14cf4a4704698fbd52c597db3484ecad4
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-01-26 19:25:51 +0000

    updated

--- functions/try-it/sayHello.js
+++ functions/try-it/sayHello.js
@@ -1,11 +1,27 @@
+<<<<<<< HEAD
+// function sayHello() {
+//    console.log("Hello, World!");
+// }
+
+function sumToN(n) {
+   let sum = 0;
+   for (let i = 0; i <= n; i++) {
+       sum += i;
+   }
+   return sum;
+=======
 // function sayHello() {
 //    console.log("Hello, World!");
 // }
 
 function plusTwo(num) {
     return num + 2;
+>>>>>>> 5e9132d45566ad001ffabea743905b2bddbbcebf
 }
 
+<<<<<<< HEAD
+console.log(sumToN(3));
+=======
 let a = 2;
 
 for (let i=0; i < 4; i++) {
@@ -29,3 +45,4 @@ console.log(a);
 // }
 
 // console.log(isEven(43));
+>>>>>>> 5e9132d45566ad001ffabea743905b2bddbbcebf
