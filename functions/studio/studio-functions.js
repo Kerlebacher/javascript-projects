a3190c51f94ff24960a4abf3a5638034fdccec96
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-01-26 02:01:56 +0000

    Studio complete

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -25,46 +25,56 @@ function reverseCharacters(str) {
 
 function reverseCharacters(str) {
     
-    if (typeof(str) === 'string') {
+    if (typeof str === "string") {
         let reversed = (str.split('').reverse().join(''));
-        // console.log(typeof(reversed));
-        return reversed;
+               return reversed;
         
     } else {
         let reversed = (str.toString().split('').reverse().join(''));
         reversed = Number(reversed);
-        console.log(typeof(reversed));
-     return reversed;
+             return reversed;
         }  
 }
-console.log(reverseCharacters(42));
+// console.log(reverseCharacters(124));
 
 
 // Part Three: Complete Reversal
 
-// 1. Define and initialize an empty array.
-// 2. Loop through the old array.
-// 3. For each element in the old array, call reverseCharacters to flip the characters or digits.
-// 4. Add the reversed string (or number) to the array defined in part ‘a’.
-// 5. Return the final, reversed array.
-// 6. Be sure to print the results from each test case in order to verify your code.
+//Now we are ready to finish our complete reversal process. Create a new function with one parameter, which is the array we want to change. The function should:
+
+// Define and initialize an empty array.
+// Loop through the old array.
+// For each element in the old array, call reverseCharacters to flip the characters or digits.
+// Add the reversed string (or number) to the array defined in part ‘a’.
+// Return the final, reversed array.
+// Be sure to print the results from each test case in order to verify your code.
 
 let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
 let arrayTest3 = ['hello', 'world', 123, 'orange'];
 
-let arr = []
+function reverseArray(arr) {
+let emptyarr = []
+for (let i = 0; i < arr.length; i++) {
+    emptyarr[i] = reverseCharacters(arr[i])
+}
+return emptyarr.reverse();
+}
+console.log (reverseArray(arrayTest3));
+
 // Bonus Missions
 
-// 1. Have a clear, descriptive name like funPhrase.
-// 2. Retrieve only the last character from strings with lengths of 3 or less.
-// 3. Retrieve only the first 3 characters from strings with lengths larger than 3.
-// 4. Use a template literal to return the phrase We put the '___' in '___'. Fill the first blank with the modified string, and fill the second blank with the original string.
+// Define a function with one parameter, which will be a string. The function must do the following:
+
+// Have a clear, descriptive name like funPhrase.
+// Retrieve only the last character from strings with lengths of 3 or less.
+// Retrieve only the first 3 characters from strings with lengths larger than 3.
+// Use a template literal to return the phrase We put the '___' in '___'. Fill the first blank with the modified string, and fill the second blank with the original string.
 
 // Test Function
 
-// 1. Outside of the function, define the variable str and initialize it with a string (e.g. 'Functions rock!').
-// 2. Call your function and print the returned phrase.
+// Outside of the function, define the variable str and initialize it with a string (e.g. 'Functions rock!').
+// Call your function and print the returned phrase.
 
 // Area of rectangle equal to length x width
 
