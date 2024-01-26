5e9132d45566ad001ffabea743905b2bddbbcebf
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-01-26 07:30:09 -0600

    Update part-three-number-sorting-easy-way.js

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -1,3 +1,20 @@
+/*
+If you Google “JavaScript sort array of numbers” (or something similar), many options appear, and they all give pretty much the same result. The sites just differ in how much detail they provide when explaining the solution.
+
+One reference is here: W3Schools.
+
+End result: the JavaScript syntax for numerical sorting is arrayName.sort(function(a, b){return a-b});.
+
+Here, the anonymous function determines which element is larger and swaps the positions if necessary. This is all that sort needs to order the entire array.
+
+Using the syntax listed above:
+
+Sort each sample array in increasing order.
+Sort each sample array in decreasing order.
+Does the function alter arrayName?
+Did your sorting function from part B alter arrayName?
+*/
+
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
