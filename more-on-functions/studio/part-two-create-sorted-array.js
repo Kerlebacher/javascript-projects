d8782d81642ee7a5a7b5b95c20f8bc52cdac2c58
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-01-26 07:29:16 -0600

    Update part-two-create-sorted-array.js

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -11,12 +11,12 @@ function findMinValue(arr){
 //Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
 
 /*Within the function:
-1) Define a new, empty array to hold the final sorted numbers.
-2) Use the findMinValue function to find the minimum value in the old array.
-3) Add the minimum value to the new array, and remove the minimum value from the old array.
-4) Repeat parts b & c until the old array is empty.
-5) Return the new sorted array.
-6) Be sure to print the results in order to verify your code.*/
+1. Define a new, empty array to hold the final sorted numbers.
+2. Use your function from the previous exercise to find the minimum value in the old array.
+3. Add the minimum value to the new array, and remove the minimum value from the old array.
+4. Repeat parts b & c until the old array is empty.
+5. Return the new sorted array.
+6. Be sure to print the results in order to verify your code.*/
 
 //Your function here...
 
