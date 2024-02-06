2000062848a356cd202bf1d836d33841bbe99d22
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-02-06 00:53:14 +0000

    done

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,4 @@
+const isPalindrome = require('./practiceExports');
+const isEvenOrOdd = require('./practiceExports');
+const randomArrayElement = require('./practiceExports');
 // Import the modules exported from practiceExports.js below:
