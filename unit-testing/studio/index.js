37a99e794ba2d72eef5b38dc7cacae3a49d21683
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-02-08 09:04:20 -0600

    Update index.js

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,18 @@
 
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"]
+
+};
+function launchOutput() {
+
+};
+
+module.exports = {
+    launchcode : launchcode,
+    launchOutput :  launchOutput
 }
 
-module.exports = launchcode;
 
