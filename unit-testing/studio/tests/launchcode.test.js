0f47ff8160a7aacc54b3b9e7b4116ce32343b87f
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-02-08 11:05:20 -0600

    Update launchcode.test.js

--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -12,7 +12,7 @@ describe("launchcode", function(){
 describe("launchcode", function(){
 
   test("The value of executiveDirector responds with 'Jeff'", function() {
-  let output = launchcode(launchcode.executiveDirector);
+  let output = executiveDirector();
   expect(output).toEqual("Jeff");
 });
 });
@@ -20,7 +20,7 @@ describe("launchcode", function(){
 describe("launchcode", function(){
 
   test("The value of percentageCoolEmployees responds with '100'", function() {
-  let output = launchcode(launchcode.percentageCoolEmployees);
+  let output = percentageCoolEmployees();
   expect(output).toEqual(100);
 });
 });
@@ -28,12 +28,7 @@ describe("launchcode", function(){
 describe("launchcode", function(){
 
   test("The value of programsOffered responds with 'Web Development' in idex 0, 'Data analysis' in index 1, and '' in index 2 ", function() {
-  let output0 = launchcode(launchcode.programsOffered[0]);
-  expect(output0).toEqual("Web Development");
-  let output1 = launchcode(launchcode.programsOffered[1]);
-  expect(output1).toEqual("Data Analysis");
-  let output2 = launchcode(launchcode.programsOffered[2]);
-  expect(output2).toEqual("Liftoff");
+expect(programsOffered).toEqual(expect.arrayContaining(["Web Development", "Data Analysis", "Liftoff"]));
 });
 });
 
