8a002682339155b1a30e991cdbee5d9b06f371c7
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-02-12 05:15:16 +0000

    updated

--- classes/exercises/ClassExercises.js
+++ classes/exercises/ClassExercises.js
@@ -1,10 +1,45 @@
 // Define your Book class here:
+class book {
 
+    constructor(title, auhor, copyDate, ISBN, pages, checkoutNum, isdiscarded = false){
+        this.title = title;
+        this.author = author;
+        this.copyDate = copyDate;
+        this.ISBN = ISBN;
+        this.pages = Pages;
+        this.checkoutNum = checkoutNum;
+        this.isdiscarded = isdiscarded;
+    }
+
+    checkout(used=1) {
+        this.checkoutNum += used;
+    }
+}
 
 // Define your Manual and Novel classes here:
+class Manual extends Book {
+    constructor(title, auhor, copyDate, ISBN, pages, checkoutNum, isdiscarded = false){
+        super(title, auhor, copyDate, ISBN, pages, checkoutNum, isdiscarded = false);
+    }
+    dispose(currentYear){
+        if (currentYear - this.copyDate > 5) {
+            this.isdiscarded = true;
+        }
+    }
+}
 
-
+class Novel extends Book {
+    constructor(title, auhor, copyDate, ISBN, pages, checkoutNum, isdiscarded = false){
+        super(title, auhor, copyDate, ISBN, pages, checkoutNum, isdiscarded = false);
+    }
+    dispose(){
+        if (checkoutNum > 100) {
+            this.isdiscarded = true;
+        }
+    }
+}
 // Declare the objects for exercises 2 and 3 here:
-
+let prideAndPrejudice = new Novel("Pride and Prejudice", "Jane Austen", 1813, 111111111111, 432, 32,);
+let topSecretShuttleBuildingManual = new Manual("Top Secret Shuttle Building Manual", "Redacted", 2013, 0000000000000, 1147, 1)
 
 // Code exercises 4 & 5 here:
--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -1,9 +1,53 @@
 //Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
+class CrewCandidate {
+constructor(name, mass, scores) {
+    this.name = name;
+    this.mass = mass;
+    this.scores = scores;
+}
+addScore(score) {
+    this.scores.push(score);
+}
+average() {
+    let sum = 0;
+    for (let j = 0; j < this.scores.length; j++) {
+        sum += this.scores[j];
+    }
+    return Math.round(10 * sum / this.scores.length) / 10;
+}
+
+status() {
+    let avg = this.average();
+    if(avg >= 90) {
+        return "Accepted";
+    } else if(avg >= 80) {
+        return "Reserve";
+    } else if(avg >= 70) {
+        return "Probationary";
+    } else {
+        return "Rejected";
+    }
+
+}
+}
+
+let bubba = new CrewCandidate("Bubba Bear", 135, [88, 85, 90]);
+let merry = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
+let glad = new CrewCandidate("Glad Gator", 225, [75, 78, 62]);
 
 
+// console.log(bubba);
+// console.log(merry);
+// console.log(glad);
 
 //Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
+// bubba.addScore(83);
+// console.log(bubba.scores);
 
+// console.log(merry.average());
 
-
+let candidates = [bubba, merry, glad];
+for (let i=0; i <  candidates.length; i++) {
+    console.log(`\n${candidates[i].name} earned an average test score of ${candidates[i].average()}% and has a status of ${candidates[i].status()}.`);
+}
 //Part 4 - Use the methods to boost Glad Gator’s status to Reserve or higher. How many tests will it take to reach Reserve status? How many to reach Accepted? Remember, scores cannot exceed 100%.
