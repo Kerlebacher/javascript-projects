a4f87ddb39d8d1d03f952026df1b10950436d7b2
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-03-05 02:04:59 +0000

    updating

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -3,17 +3,22 @@
 
           fetch("https://handlers.education.launchcode.org/static/astronauts.json").then((res) => {
              res.json().then((data) => {
-               console.log(data);
+            //  data.sort(function  (a,b) {
+            //     return a.hoursInSpace < b.hoursInSpace? 1 :-1;
+            //  });
+
+
                for (let i=0; i < data.length; i++) {
                 let id = data[i];
-                let image = 
+                // let isActive = id.active ? "active" : "";
                 container.innerHTML += `
+         
                 <div class="astronaut">
                   <div class="bio">
                     <h3> ${id.firstName} ${id.lastName} </h3>
                     <ul>
                         <li>Hours in Space: ${id.hoursInSpace}</li>
-                        <li>Active: ${id.active}</li>
+                        <li class="${isActive}">Active: ${id.active}</li>
                         <li>Skills: ${id.skills.join(", ")}</li>
                     </ul>
                   </div>
