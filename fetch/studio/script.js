dc94dc8ee5188d33e8f1081679d2c921c6685f5a
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-03-05 01:00:20 +0000

    Studio update

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,2 +1,26 @@
-//TODO: Add Your Code Below
- 
+      window.addEventListener("load", function() {     
+        const container =  document.getElementById('container');
+
+          fetch("https://handlers.education.launchcode.org/static/astronauts.json").then((res) => {
+             res.json().then((data) => {
+               console.log(data);
+               for (let i=0; i < data.length; i++) {
+                let id = data[i];
+                let image = 
+                container.innerHTML += `
+                <div class="astronaut">
+                  <div class="bio">
+                    <h3> ${id.firstName} ${id.lastName} </h3>
+                    <ul>
+                        <li>Hours in Space: ${id.hoursInSpace}</li>
+                        <li>Active: ${id.active}</li>
+                        <li>Skills: ${id.skills.join(", ")}</li>
+                    </ul>
+                  </div>
+                  <img class="avatar" src"${id.picture}">
+                </div>
+                `;
+               }
+              });
+          });
+        });
