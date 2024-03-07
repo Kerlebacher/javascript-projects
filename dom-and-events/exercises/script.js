32f4b3a751c4cc4842321c3ce7cbeafea2505d4d
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-03-07 19:48:52 +0000

    Dom exercises complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,26 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+    button.addEventListener("click", event => {
+        paragraph.innerHTML = "Huston! We have liftoff!"
+    });
+
+    missionAbort.addEventListener('mouseover', event =>{
+        missionAbort.style.backgroundColor = 'red';
+    });
+
+    missionAbort.addEventListener('mouseout', event =>{
+        missionAbort.style.backgroundColor = 'unset';
+    });
+    missionAbort.addEventListener('click', event =>{
+        let confirmation = window.confirm("Are you sure you want to abort the mission?");
+        if (confirmation) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+        }
+    });
 }
 
 window.addEventListener("load", init);
+
+
+ 
